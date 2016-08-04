c065f5de55143a4a3bf46d9025a136b4151e8ef4
welefen <welefen@gmail.com>
2016-08-04 18:05:04 +0800

    Use stc 2

--- build.sh
+++ build.sh
@@ -1,40 +1,16 @@
 #!/bin/sh
-STC_PATH="/Users/welefen/Develop/git/stc/src"
-path=`dirname $0`;
-first=${path:0:1};
-if [[ $first != '/' ]];then
-    path=$(pwd);
-fi
 
-if [ -d ${path}"/output" ];then
-	rm -rf ${path}"/output";
-fi
-mkdir ${path}"/output";
-if [ ! -f ${path}"/config.php" ];then
-	cp $STC_PATH/config/config.php ${path};
-fi
+rm -rf output;
+mkdir output;
 
-
-rm -rf www/static/other/icon/*.bak;
-
-if [ -d "view_build" ];then
-  rm -rf view_build/;
-fi
-
+rm -rf view_build/;
 mkdir view_build;
 cp -r view/* view_build;
-
-
 
 node www/production.js home/generate/single;
 node www/production.js home/generate/html;
 
-#path=$(pwd);
-/usr/local/bin/php $STC_PATH/index.php ${path} test $1;
-if [ -f ${path}"/stc.error.log" ]; then
-    rm -rf ${path}"/stc.error.log";
-    #exit 1;
-fi
+DEBUG=pluginMatchedFiles,pluginTotalTime node stc.config.js;
 
 rm -rf view_build;
 cd output;
@@ -53,15 +29,5 @@ cp -r app output/;
 cp -r ssl output/;
 
 cd output;
-rm -rf www/static/other/icon/*
-
-tar zcvf ../output.tar.gz *;
-cd ..
-
-
-
-
-#sleep 2;
-
-#wget https://www.thinkjs.org/doc.html;
-#rm -rf doc.html*;
+rm -rf www/static/other/icon/*;
+tar zcf ../output.tar.gz *;
---
+++ stc.config.js
@@ -0,0 +1,48 @@
+var stc = require('stc');
+var htmlCompress = require('stc-html-compress');
+var uglify = require('stc-uglify');
+var cssCompress = require('stc-css-compress');
+var cdn = require('stc-cdn');
+var cdnAdapter = require('@q/stc-cdn-qstatic');
+var localstorage = require('stc-localstorage');
+var localstorageAdapter = require('stc-localstorage-ejs');
+
+stc.config({
+  include: ['www/static', 'view_build'],
+  exclude: [/\.bak$/],
+  tpl: {
+    engine: 'ejs',
+    ld: ['<%'],
+    rd: ['%>'],
+  }
+});
+
+stc.workflow({
+  uglify: {plugin: uglify},
+  cssCompress: {
+    plugin: cssCompress,
+    exclude: [/\.min\./]
+  },
+  htmlCompress: {plugin: htmlCompress},
+  localstorage: {
+    include: [{type: 'tpl'}],
+    exclude: [/\/doc\//],
+    plugin: localstorage,
+    options: {
+      adapter: localstorageAdapter,
+      minLength : 200,
+      appId : 'd0ac6c56'
+    }
+  },
+  cdn: {
+    plugin: cdn, 
+    exclude: [/\/doc\//],
+    options: {
+      adapter: cdnAdapter, 
+      https: true,
+      exclude: [/other\/icon/]
+    }
+  }
+});
+
+stc.start();
