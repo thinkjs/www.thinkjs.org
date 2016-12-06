0cb2cf001e1a01edd47bb548c701c4378752709e
welefen <welefen@gmail.com>
2016-12-06 18:13:27 +0800

    update

--- renew_cert.sh
+++ renew_cert.sh
@@ -1,4 +1,4 @@
-cd /home/welefen/www/www.thinkjs.org/lets
+cd /home/welefen/www/www.thinkjs.org/ssl
 python acme_tiny.py --account-key account.key --csr domain.csr --acme-dir /home/welefen/www/www.thinkjs.org/www/challenges/ > signed.crt || exit
 wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > intermediate.pem
 cat signed.crt intermediate.pem > chained.pem
