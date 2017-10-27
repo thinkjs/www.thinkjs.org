408324046d8b13d912ce85a43a2335e994a74003
welefen <welefen@gmail.com>
2017-10-27 18:50:42 +0800

    update

--- stc.config.js
+++ stc.config.js
@@ -4,8 +4,8 @@ var uglify = require('stc-uglify');
 var cssCompress = require('stc-css-compress');
 //var cdn = require('stc-cdn');
 //var cdnAdapter = require('@q/stc-cdn-qstatic');
-var localstorage = require('stc-localstorage');
-var localstorageAdapter = require('stc-localstorage-ejs');
+// var localstorage = require('stc-localstorage');
+// var localstorageAdapter = require('stc-localstorage-ejs');
 var version = require('stc-resource-version');
 
 stc.config({
@@ -28,16 +28,16 @@ stc.workflow({
   },
   
   htmlCompress: {plugin: htmlCompress},
-  localstorage: {
-    include: [{type: 'tpl'}],
-    exclude: [/\/doc\//],
-    plugin: localstorage,
-    options: {
-      adapter: localstorageAdapter,
-      minLength : 200,
-      appId : 'd0ac6c56'
-    }
-  },
+  // localstorage: {
+  //   include: [{type: 'tpl'}],
+  //   exclude: [/\/doc\//],
+  //   plugin: localstorage,
+  //   options: {
+  //     adapter: localstorageAdapter,
+  //     minLength : 200,
+  //     appId : 'd0ac6c56'
+  //   }
+  // },
   version: {
     plugin: version,
     include: [{type: 'tpl'}]
